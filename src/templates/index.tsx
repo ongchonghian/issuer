877580716ad75349d84ffc7e33e2380d6ace1db2
ongchonghian <chonghian.ong@gmail.com>
2020-08-27 11:00:24 +0800

    Update index.tsx

--- src/templates/index.tsx
+++ src/templates/index.tsx
@@ -1,7 +1,7 @@
 import { TemplateRegistry } from "@govtechsg/decentralized-renderer-react-components";
 import { templates as blTemplates } from "./billOfLading";
 import styled from "@emotion/styled";
-import { SGInvoiceTemplate } from "./PEPPOLTemplate";
+//import { SGInvoiceTemplate } from "./PEPPOLTemplate";
 export const registry: TemplateRegistry<any> = {    
   BILL_OF_LADING: blTemplates
 };
