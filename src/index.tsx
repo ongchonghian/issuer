391d3b9a0d7ed50a0afcf15934fa233ce73e0bbf
ongchonghian <chonghian.ong@gmail.com>
2020-08-27 11:39:53 +0800

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { FramedDocumentRenderer } from "@govtechsg/decentralized-renderer-react-components";
+import { FramedDocumentRenderer, fullAttachmentRenderer } from "@govtechsg/decentralized-renderer-react-components";
 import { registry } from "./templates";
+import "./index.css";
 
-ReactDOM.render(<FramedDocumentRenderer templateRegistry={registry} />, document.getElementById("root"));
+ReactDOM.render(
+  <FramedDocumentRenderer templateRegistry={registry} attachmentToComponent={fullAttachmentRenderer} />,
+  document.getElementById("root")
+);
